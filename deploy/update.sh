92f5730326f02a1699e8a9d84fb509b8c696bac0
Jiahao Li <lijh15328076@gmail.com>
2025-07-05 02:37:08 +0900

    Modify update.sh

--- deploy/update.sh
+++ deploy/update.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 
-set -e
+set -e  # Exit on any error
 
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
+# Ensure distutils is installed (optional if already done in setup)
+sudo apt-get install -y python3-distutils
+
+# Navigate to project directory
 cd $PROJECT_BASE_PATH
+
+# Pull latest changes
 git pull
+
+# Use virtual environment explicitly to run Django commands
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
+
+# Restart app via supervisor
 supervisorctl restart profiles_api
 
 echo "DONE! :)"
